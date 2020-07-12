32231515c0b209fef0db18e20b9360dc34a55dc5
m-oka-system <m-oka-system@outlook.com>
2020-07-12 22:33:31 +0900

    [update]プローブと負荷分散規則の作成を追加

--- vmss/create-vmss.sh
+++ vmss/create-vmss.sh
@@ -7,7 +7,7 @@ rgName="w-iaas-rg"
 location="japanwest"
 vmssName="w-iaas-vmss"
 imageName="w-iaas-win2016-ja-with-azmodule-20200308144738"
-imageId=$(az image show --resource-group $rgName --name $imageName --query id --output tsv) && echo $imageId
+imageId=$(az image show --resource-group $rgName --name $imageName --query id --output tsv)
 sku="Standard_A1_v2"
 adminUser="cloudadmin"
 adminPassword="My5up3rStr0ngPaSw0rd!"
@@ -16,14 +16,14 @@ vnetName="w-iaas-vnet"
 subnetName="web-subnet"
 lbName=${vmssName}-lb
 lbPublicIpAddressName=${lbName}-ip
-lbBackendPoolName="bepool"
+lbBackendPoolName=${lbName}-bepool
 lbSku="Basic"
 lbNatPoolName=${lbName}-natpool
 backendPort=3389
 probeName=${lbName}-probe
 instanceCount=1
 
-# Create vmss
+# Create vmss with Load Balancer
 az vmss create --resource-group $rgName --location $location \
   --name $vmssName \
   --image $imageName \
@@ -44,3 +44,24 @@ az vmss create --resource-group $rgName --location $location \
   --scale-in-policy Default \
   --upgrade-policy-mode Manual \
   --single-placement-group true
+
+# Create a probe
+az network lb probe create \
+  --resource-group $rgName \
+  --name $probeName \
+  --lb-name $lbName \
+  --protocol Tcp \
+  --port 80 \
+  --interval 5
+
+# Create a load balancing rule
+az network lb rule create \
+  --resource-group $rgName \
+  --name ${lbName}-rule \
+  --lb-name $lbName \
+  --frontend-ip-name loadBalancerFrontEnd \
+  --protocol tcp \
+  --frontend-port 80 \
+  --backend-port 80 \
+  --backend-pool-name $lbBackendPoolName \
+  --probe-name $probeName
