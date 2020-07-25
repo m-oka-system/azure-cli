b07db84e2d15c7fb71c1f66019a385d850b2e64e
m-oka-system <m-oka-system@outlook.com>
2020-07-25 22:02:32 +0900

    [clean]リファクタリング

--- keyvault/create-keyvault-certificate.sh
+++ keyvault/create-keyvault-certificate.sh
@@ -9,6 +9,11 @@ vaultName="e-paas-vault"
 selfCertName="selfcert"
 fqdn="www.example.com"
 
+# Create a key vault.
+az keyvault create --resource-group $rgName --location $location \
+  --name $vaultName \
+  --enable-soft-delete false
+
 # Create a self-signed certificate.
 sed -e "s/INPUT_YOUR_FQDN/$fqdn/g" selfcert_policy.org.json > selfcert_policy.json
 az keyvault certificate create --vault-name $vaultName --name $selfCertName --policy @selfcert_policy.json
