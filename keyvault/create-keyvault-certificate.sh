38930c805c8110cc4eee83802dceecadadaffd9f
m-oka-system <m-oka-system@outlook.com>
2020-07-25 23:17:08 +0900

    [update]KeyVaultへのアクセス設定を追加

--- keyvault/create-keyvault-certificate.sh
+++ keyvault/create-keyvault-certificate.sh
@@ -17,3 +17,9 @@ az keyvault create --resource-group $rgName --location $location \
 # Create a self-signed certificate.
 sed -e "s/INPUT_YOUR_FQDN/$fqdn/g" selfcert_policy.org.json > selfcert_policy.json
 az keyvault certificate create --vault-name $vaultName --name $selfCertName --policy @selfcert_policy.json
+
+# Allow AppService service principals access to Key Vault
+az keyvault set-policy --name $vaultName \
+  --spn abfa0a7c-a6b6-4736-8310-5855508787cd \
+  --secret-permissions get \
+  --certificate-permissions get
